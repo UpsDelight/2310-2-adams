fce3c384d577b43df30fcaeda2b09f97a0ebc110
ayasa-nakashima <kogeusagi12@gmail.com>
2023-10-27 15:54:10 +0900

    :recycle: ご指摘事項修正

--- assets/js/survey-monitor/index.js
+++ assets/js/survey-monitor/index.js
@@ -1,24 +1,40 @@
 $(function () {
+    /**
+     * アコーディオン
+     */
     let accordionDetails = '.js-accordion';
     let accordionSummary = '.js-summary';
     let accordionContent = '.js-content';
     let speed = 500;
 
-    $(accordionSummary).each(function (){
-        $(this).on("click",function (event){
+    $(accordionSummary).each(function () {
+        $(this).on("click", function (event) {
             // デフォルトの挙動を無効化
             event.preventDefault();
-            if($(this).parent($(accordionDetails)).attr("open")){
+            if ($(this).parent($(accordionDetails)).attr("open")) {
                 // アコーディオンを閉じるときの処理
-                $(this).nextAll($(accordionContent)).slideUp(speed,function (){
+                $(this).nextAll($(accordionContent)).slideUp(speed, function () {
                     $(this).parent($(accordionDetails)).removeAttr("open");
                     $(this).show();
                 });
-            }else{
+            } else {
                 // アコーディオンを開くときの処理
-                $(this).parent($(accordionDetails)).attr("open","true");
+                $(this).parent($(accordionDetails)).attr("open", "true");
                 $(this).nextAll($(accordionContent)).hide().slideDown(speed);
             }
         })
-    })
+    });
+
+    /**
+     * スムーススクロール
+     */
+    $(document).on('click', 'a[href*="#"]', function () {
+        let time = 500;
+        let header = $('header').innerHeight();
+        let target = $(this.hash);
+        if (!target.length) return;
+        let targetY = target.offset().top - header;
+        $('html,body').animate({ scrollTop: targetY }, time, 'swing');
+        return false;
+    });
 });
